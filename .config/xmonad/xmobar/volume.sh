084c82d3cc067d5e32e5c55abffb6d6ce9bab510
Scherso <90007553+Scherso@users.noreply.github.com>
2022-07-26 17:35:04 -0400

    volume.sh changes

--- .config/xmonad/xmobar/volume.sh
+++ .config/xmonad/xmobar/volume.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
+INPUTMUTE=$(pulseaudio-ctl full-status | awk '{print $3}')
 MUTE=$(pulseaudio-ctl full-status | awk '{print $2}')
 VOLUME=$(pulseaudio-ctl full-status | awk '{print $1}')
 
-if [ "$MUTE" = "yes" ]; then
-    echo "<fc=#6B7089,#4a5260:0> \ </fc>"
-elif [ "$MUTE" = "no" ] && [ "$VOLUME" -eq 0 ]; then
-    echo "<fc=#6B7089,#4a5260:0> $VOLUME% </fc>"
-else
-    echo " $VOLUME%"
+if [ "$MUTE" = "yes" ] && [ "$INPUTMUTE" = "yes" ]; then
+    echo "<fc=#6B7089,#4a5260:0> </fc><fc=#6B7089,#4a5260:0></fc> "
+elif [ "$MUTE" = "yes" ] && [ "$INPUTMUTE" = "no" ]; then
+    echo "<fc=#6B7089,#4a5260:0> </fc> "
+elif [ "$MUTE" = "no" ] && [ "$VOLUME" -eq 0 ] && [ "$INPUTMUTE" = "yes" ]; then
+    echo "<fc=#6B7089,#4a5260:0> $VOLUME% </fc><fc=#6B7089,#4a5260:0></fc> "
+elif [ "$MUTE" = "no" ] && [ "$VOLUME" -eq 0 ] && [ "$INPUTMUTE" = "no" ]; then
+    echo "<fc=#6B7089,#4a5260:0> $VOLUME% </fc> "
+elif [ "$MUTE" = "no" ] && [ "$VOLUME" -gt 0 ] && [ "$INPUTMUTE" = "no" ]; then
+    echo " $VOLUME% "
+elif [ "$MUTE" = "no" ] && [ "$VOLUME" -gt 0 ] && [ "$INPUTMUTE" = "yes" ]; then
+    echo " $VOLUME% <fc=#6B7089,#4a5260:0></fc>"
 fi
