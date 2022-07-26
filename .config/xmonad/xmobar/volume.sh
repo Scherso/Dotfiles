42496f574f631ec65a6c868836795729363a6f1c
Scherso <90007553+Scherso@users.noreply.github.com>
2022-07-26 13:22:39 -0400

    XMobar changes

--- .config/xmonad/xmobar/volume.sh
+++ .config/xmonad/xmobar/volume.sh
@@ -4,9 +4,9 @@ MUTE=$(pulseaudio-ctl full-status | awk '{print $2}')
 VOLUME=$(pulseaudio-ctl full-status | awk '{print $1}')
 
 if [ "$MUTE" = "yes" ]; then
-    echo "<fc=#696B71,#545d6d:0> \ </fc>"
+    echo "<fc=#6B7089,#4a5260:0> \ </fc>"
 elif [ "$MUTE" = "no" ] && [ "$VOLUME" -eq 0 ]; then
-    echo "<fc=#696B71,#545d6d:0> $VOLUME% </fc>"
+    echo "<fc=#6B7089,#4a5260:0> $VOLUME% </fc>"
 else
     echo " $VOLUME%"
 fi
