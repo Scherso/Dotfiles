175efea74c95da19bfcc2db2848bd08990784d39
Scherso <90007553+Scherso@users.noreply.github.com>
2022-08-13 13:47:38 -0400

    switch to pipewire

--- .config/xmonad/xmobar/volume.sh
+++ .config/xmonad/xmobar/volume.sh
@@ -1,19 +1,12 @@
 #!/bin/bash
 
-INPUTMUTE=$(pulseaudio-ctl full-status | awk '{print $3}')
-MUTE=$(pulseaudio-ctl full-status | awk '{print $2}')
-VOLUME=$(pulseaudio-ctl full-status | awk '{print $1}')
+MUTE=$(pactl get-sink-mute @DEFAULT_SINK@ | awk '{print $2}')
+VOLUME=$(pactl get-sink-volume @DEFAULT_SINK@ | awk '{print $5}' | sed 's/%//')
 
-if [ "$MUTE" = "yes" ] && [ "$INPUTMUTE" = "yes" ]; then
-    echo "<fc=#6B7089,#4a5260:0> </fc><fc=#6B7089,#4a5260:0></fc> "
-elif [ "$MUTE" = "yes" ] && [ "$INPUTMUTE" = "no" ]; then
-    echo "<fc=#6B7089,#4a5260:0> </fc> "
-elif [ "$MUTE" = "no" ] && [ "$VOLUME" -eq 0 ] && [ "$INPUTMUTE" = "yes" ]; then
-    echo "<fc=#6B7089,#4a5260:0> $VOLUME% </fc><fc=#6B7089,#4a5260:0></fc> "
-elif [ "$MUTE" = "no" ] && [ "$VOLUME" -eq 0 ] && [ "$INPUTMUTE" = "no" ]; then
-    echo "<fc=#6B7089,#4a5260:0> $VOLUME% </fc> "
-elif [ "$MUTE" = "no" ] && [ "$VOLUME" -gt 0 ] && [ "$INPUTMUTE" = "no" ]; then
-    echo " $VOLUME% "
-elif [ "$MUTE" = "no" ] && [ "$VOLUME" -gt 0 ] && [ "$INPUTMUTE" = "yes" ]; then
-    echo " $VOLUME% <fc=#6B7089,#4a5260:0></fc>"
+if [ "$MUTE" = "yes" ] ; then 
+    echo "<fc=#6B7089,#4a5260:0> </fc>"
+elif [ "$MUTE" = "no" ] && [ "$VOLUME" -eq 0 ] ; then
+    echo "<fc=#6B7089,#4a5260:0> $VOLUME% </fc>"
+elif [ "$MUTE" = "no" ] && [ "$VOLUME" -gt 0 ] ; then
+    echo " $VOLUME%"
 fi
