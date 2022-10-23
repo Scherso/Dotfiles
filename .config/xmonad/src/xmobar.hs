2547c6e33c8e258e04d607df0bb614c644b7f4e4
Scherso <90007553+Scherso@users.noreply.github.com>
2022-10-23 02:37:02 -0400

    Re-format xmonad files

--- .config/xmonad/src/xmobar.hs
+++ .config/xmonad/src/xmobar.hs
@@ -3,82 +3,97 @@ import System.IO.Unsafe          (unsafeDupablePerformIO)
 
 import Xmobar
 
-myHomeDir = unsafeDupablePerformIO (getEnv "HOME") :: String
+main :: IO ()
+main = xmobar =<< myConfig
 
-myConfig :: Config
+myHomeDir :: String
+myHomeDir = unsafeDupablePerformIO (getEnv "HOME") 
+
+myConfig :: IO Config
 myConfig =
-  defaultConfig
-    { font            =   "xft:SF Mono Nerd Font:pixelsize=11:antialias=true:hinting=true" 
-    , additionalFonts = [ "xft:SF Mono Nerd Font:pixelsize=10:antialias=true:hinting=true"
-                        , "xft:SF Mono Nerd Font:size=13:antialias=true:hinting=true"
-                        , "xft:SF Mono Nerd Font:size=11:antialias=true:hinting=true"
-                        , "xft:SF Mono Nerd Font:size=11:antialias=true:hinting=true"
-                        , "xft:SF Mono Nerd Font:pixelsize=13:antialias=true:hinting=true"
-                        ]
-    , textOffsets      = [20, 22, 21, 21, 20]
-    , bgColor          = "#282C34"
-    , fgColor          = "#AAB2BF"
-    , borderColor      = "#544862"
-    , border           = FullB 
-    , borderWidth      = 1
-    {-
-    , position         = Static { xpos = 13, ypos = 1034, width = 1893, height = 32 } Bottom Padded
-    , position         = Static { xpos = 0, ypos = 1048, width = 1920, height = 32 } Bottom Flat
-    , position         = Static { xpos = 0, ypos = 0, width = 1920, height = 32 } Top Flat
-    -}
-    , position         = Static { xpos = 13, ypos = 8, width = 1893, height = 32 }
-    , alpha            = 255
-    , overrideRedirect = False
-    , lowerOnStart     = True
-    , hideOnStart      = False
-    , allDesktops      = True
-    , persistent       = True
-    , iconRoot         = myHomeDir <> "/.config/xmonad/icons"
-    , iconOffset       = -1
-    , commands         = myCommands
-    , sepChar  = "%"
-    , alignSep = "}{"
-    , template = " <fn=2><fc=#31353F,#282C34:7>\xe0b6</fc></fn>\
-                 \<fn=4><fc=#C678DD,#31353F:5>\xf30d </fc></fn>\
-                 \<fn=2><fc=#31353F,#282C34:7>\xe0b4</fc></fn> \
-                 \\
-                 \<fn=5>%UnsafeXMonadLog%</fn>}{\
-                 \<fn=2><fc=#31353F,#282C34:7>\xe0b6</fc></fn>\
-                 \<fn=4><fc=#E06C75,#31353F:5>%wlp5s0%</fc></fn>\
-                 \<fn=2><fc=#31353F,#282C34:7>\xe0b4</fc></fn> \
-                 \\
-                 \<fn=2><fc=#31353F,#282C34:7>\xe0b6</fc></fn>\
-                 \<fn=4><fc=#56B6C2,#31353F:5>%k10temp%</fc></fn>\
-                 \<fn=2><fc=#31353F,#282C34:7>\xe0b4</fc></fn> \
-                 \\
-                 \<fn=2><fc=#31353F,#282C34:7>\xe0b6</fc></fn>\
-                 \<fn=4><fc=#C678DD,#31353F:5>%gpu%</fc></fn>\
-                 \<fn=2><fc=#31353F,#282C34:7>\xe0b4</fc></fn> \
-                 \\
-                 \<fn=2><fc=#31353F,#282C34:7>\xe0b6</fc></fn>\
-                 \<fn=4><fc=#98C379,#31353F:5>%vol%</fc></fn>\
-                 \<fn=2><fc=#31353F,#282C34:7>\xe0b4</fc></fn> \
-                 \\
-                 \<fn=2><fc=#31353F,#282C34:7>\xe0b6</fc></fn>\
-                 \<fn=4><fc=#61AFEF,#31353F:5>%date%</fc></fn>\
-                 \<fn=2><fc=#31353F,#282C34:7>\xe0b4</fc></fn> "
-    }
+    do
+        pure baseConfig
+            { template = concat $ 
+                [ " <fn=2><fc=#31353F,#282C34:7>\xe0b6</fc></fn>\
+                  \<fn=4><fc=#C678DD,#31353F:5>\xf30d </fc></fn>\
+                  \<fn=2><fc=#31353F,#282C34:7>\xe0b4</fc></fn> "
+                ] 
+                <>
+                [ "<fn=5>%UnsafeXMonadLog%</fn>}{" ] 
+                <>
+                [ "<fn=2><fc=#31353F,#282C34:7>\xe0b6</fc></fn>\
+                  \<fn=4><fc=#E06C75,#31353F:5>%wlp5s0%</fc></fn>\
+                  \<fn=2><fc=#31353F,#282C34:7>\xe0b4</fc></fn> "
+                ] 
+                <>
+                [ "<fn=2><fc=#31353F,#282C34:7>\xe0b6</fc></fn>\
+                   \<fn=4><fc=#56B6C2,#31353F:5>%k10temp%</fc></fn>\
+                   \<fn=2><fc=#31353F,#282C34:7>\xe0b4</fc></fn> "
+                ] 
+                <>
+                [ "<fn=2><fc=#31353F,#282C34:7>\xe0b6</fc></fn>\
+                   \<fn=4><fc=#C678DD,#31353F:5>%gpu%</fc></fn>\
+                   \<fn=2><fc=#31353F,#282C34:7>\xe0b4</fc></fn> "
+                ] 
+                <>
+                [ "<fn=2><fc=#31353F,#282C34:7>\xe0b6</fc></fn>\
+                   \<fn=4><fc=#98C379,#31353F:5>%vol%</fc></fn>\
+                   \<fn=2><fc=#31353F,#282C34:7>\xe0b4</fc></fn> "
+                ] 
+                <>
+                [ "<fn=2><fc=#31353F,#282C34:7>\xe0b6</fc></fn>\
+                   \<fn=4><fc=#61AFEF,#31353F:5>%date%</fc></fn>\
+                   \<fn=2><fc=#31353F,#282C34:7>\xe0b4</fc></fn> "
+                ]
+            , commands = myCommands
+            } 
 
 myCommands :: [Runnable]
 myCommands = 
-  [ Run UnsafeXMonadLog
-  , Run $ Network "wlp5s0" 
-  [ "-t"
-  , "\xf433 <rx> kb \xf431 <tx> kb"
-  ] 10
-  , Run $ K10Temp "0000:00:18.3"
-  [ "-t"
-  , "<Tdie>°C"
-  ] 10
-  , Run $ Date "\xf017 %-l:%M %p" "date" 10
-  , Run $ Com (myHomeDir <> "/.config/xmonad/scripts/gputemp.sh") ["gpu"] "gpu" 5
-  , Run $ Com (myHomeDir <> "/.config/xmonad/scripts/volume.sh" ) ["vol"] "vol" 1
-  ] 
+    [ Run UnsafeXMonadLog
+    , Run $ Network "wlp5s0" 
+    [ "-t"
+    , "\xf433 <rx> kb \xf431 <tx> kb"
+    ] 10
+    , Run $ K10Temp "0000:00:18.3"
+    [ "-t"
+    , "<Tdie>°C"
+    ] 10
+    , Run $ Date "\xf017 %-l:%M %p" "date" 10
+    , Run $ Com (myHomeDir <> "/.config/xmonad/scripts/gputemp.sh") ["gpu"] "gpu" 5
+    , Run $ Com (myHomeDir <> "/.config/xmonad/scripts/volume.sh" ) ["vol"] "vol" 1
+    ] 
 
-main :: IO ()
-main = xmobar myConfig
+baseConfig :: Config
+baseConfig =
+    defaultConfig
+        { font            =   "xft:SF Mono Nerd Font:pixelsize=11:antialias=true:hinting=true" 
+        , additionalFonts = [ "xft:SF Mono Nerd Font:pixelsize=10:antialias=true:hinting=true"
+                            , "xft:SF Mono Nerd Font:size=13:antialias=true:hinting=true"
+                            , "xft:SF Mono Nerd Font:size=11:antialias=true:hinting=true"
+                            , "xft:SF Mono Nerd Font:size=11:antialias=true:hinting=true"
+                            , "xft:SF Mono Nerd Font:pixelsize=13:antialias=true:hinting=true"
+                            ]
+        , textOffsets      = [20, 22, 21, 21, 20]
+        , bgColor          = "#282C34"
+        , fgColor          = "#AAB2BF"
+        , borderColor      = "#544862"
+        , border           = FullB 
+        , borderWidth      = 1
+        {-
+        , position         = Static { xpos = 13, ypos = 1034, width = 1893, height = 32 } Bottom Padded
+        , position         = Static { xpos = 0, ypos = 1048, width = 1920, height = 32 } Bottom Flat
+        , position         = Static { xpos = 0, ypos = 0, width = 1920, height = 32 } Top Flat
+        -}
+        , position         = Static { xpos = 13, ypos = 8, width = 1893, height = 32 }
+        , alpha            = 255
+        , overrideRedirect = False
+        , lowerOnStart     = True
+        , hideOnStart      = False
+        , allDesktops      = True
+        , persistent       = True
+        , iconRoot         = myHomeDir ++ "/.config/xmonad/icons"
+        , iconOffset       = -1
+        , sepChar  = "%"
+        , alignSep = "}{"
+        }
--- .config/xmonad/src/xmonad.hs
+++ .config/xmonad/src/xmonad.hs
@@ -1,7 +1,9 @@
 {-# LANGUAGE 
    MultiWayIf   -- Required for `toggleFull` in `myAdditionalKeys`
    , LambdaCase -- Required for `(\case)` statement in `myXmobarPP`
+   , FlexibleContexts
 #-} 
+{-# OPTIONS_GHC -Wno-missing-signatures -Wno-orphans #-}
 
 -- Data Imports 
 import qualified Data.Map                   as M
@@ -32,108 +34,125 @@ import           XMonad.Util.EZConfig       (additionalKeysP)
 import qualified XMonad.Util.Hacks          as Hacks
 import           XMonad.Util.SpawnOnce
 
+main :: IO ()
+main = do
+    xmonad
+    . docks
+    . ewmhFullscreen
+    . fullscreenSupport
+    . ewmh
+    . Hacks.javaHack
+    . withEasySB xmobar def
+    $ myConfig
+
   -- Windows key/Super key
-myModMask = mod4Mask :: KeyMask
+myModMask :: KeyMask
+myModMask = mod4Mask 
   -- Default Terminal
-myTerminal = "alacritty" :: String
+myTerminal :: String
+myTerminal = "alacritty" 
   -- Default Browser
-myBrowser = "firefox" :: String
+myBrowser :: String
+myBrowser = "firefox" 
   -- Workspaces
-myWorkspaces = map (wrap " " "" . show) [1..9] :: [String]
+myWorkspaces :: [String]
+myWorkspaces = map (wrap " " "" . show) [1 .. 9] 
   -- Border Width
-myBorderWidth = 3 :: Dimension
+myBorderWidth :: Dimension
+myBorderWidth = 3 
   -- Formal Unfocused Color
-myNormColor = "#544862" :: String
+myNormColor :: String
+myNormColor = "#544862" 
   -- Focused Color
-myFocusColor = "#61AFEF" :: String
+myFocusColor :: String
+myFocusColor = "#61AFEF" 
   -- Home Directory
-myHomeDir = unsafeDupablePerformIO (getEnv "HOME") :: String
+myHomeDir :: String
+myHomeDir = unsafeDupablePerformIO (getEnv "HOME") 
 
 myAdditionalKeys :: [(String, X ())]
 myAdditionalKeys = 
-  xmonad
-    ++ window
-    ++ applications
-    ++ multimedia
-  where 
-  -- XMonad base keybinds.
-    xmonad = 
-      [ ("M-g",       withFocused toggleBorder)
-      , ("M-S-c",     kill)
-      , ("M-S-x",     withFocused forceKillWindow)
-      , ("M-<Space>", sendMessage NextLayout)
-      , ("M-n",       refresh)
-      , ("M-S-q",     io exitSuccess)
-      , ("M-q",       spawn "xmonad --recompile ; killall xmobar ; xmonad --restart")
-      ]
-      where
-      -- Force killing a frozen window.
+    base
+        ++ window
+        ++ applications
+        ++ multimedia
+    where 
+    -- Force killing a frozen window.
         forceKillWindow :: Window -> X ()
         forceKillWindow w = withDisplay $ \d ->
-          io $ void $ killClient d w
-  -- Window management keybinds.
-    window = 
-      [ ("M-<Tab>",    windows W.focusDown)
-      , ("M-j",        windows W.focusDown)
-      , ("M-k",        windows W.focusUp)
-      , ("M-m",        windows W.focusMaster)
-      , ("M-<Return>", windows W.swapMaster)
-      , ("M-S-j",      windows W.swapDown)
-      , ("M-S-k",      windows W.swapUp)
-      , ("M-h",        sendMessage Shrink)
-      , ("M-l",        sendMessage Expand)
-      , ("M-t",        withFocused $ windows . W.sink)
-      , ("M-S-f",      withFocused toggleFull)
-      ]
-      where 
-      -- Making a window have a full float over a workspace.
+            io $ void $ killClient d w
+    -- Making a window have a full float over a workspace.
         toggleFull :: Window -> X () 
         toggleFull w = windows $ \s -> if 
-          | M.lookup w (W.floating s) == Just fullscreen -> W.sink w s 
-          | otherwise -> W.float w fullscreen s 
-            where
-              fullscreen = W.RationalRect 0 0 1 1
-  -- Spawning applications.
-    applications =
-      [ ("M-S-<Return>", spawn myTerminal)
-      , ("M-f",          spawn myBrowser)
-      , ("M-s",          spawn screenShotSelection)
-      , ("<Print>",      spawn screenShotFullscreen)
-      , ("M-p",          spawn "dmenu_run")
-      ]
-      where
+            | M.lookup w (W.floating s) == Just fullscreen -> W.sink w s 
+            | otherwise -> W.float w fullscreen s 
+                where
+                    fullscreen = W.RationalRect 0 0 1 1
+    -- Screenshots
         screenShotSelection  = "screenshot -s" :: String 
         screenShotFullscreen = "screenshot -f" :: String
-  -- Multimedia keybinds.
-    multimedia =
-      [ ("<XF86AudioPlay>",        spawn "playerctl play-pause")
-      , ("<XF86AudioPrev>",        spawn "playerctl previous")
-      , ("<XF86AudioNext>",        spawn "playerctl next")
-      , ("<XF86AudioMute>",        spawn "pactl set-sink-mute @DEFAULT_SINK@ toggle")
-      , ("<XF86AudioLowerVolume>", spawn "pactl set-sink-volume @DEFAULT_SINK@ -1.5%")
-      , ("<XF86AudioRaiseVolume>", spawn "pactl set-sink-volume @DEFAULT_SINK@ +1.5%")
-      , ("<Pause>",                spawn "amixer sset Capture toggle")
-      ]
+    -- XMonad base keybinds.
+        base = 
+            [ ("M-g",       withFocused toggleBorder)
+            , ("M-S-c",     kill)
+            , ("M-S-x",     withFocused forceKillWindow)
+            , ("M-<Space>", sendMessage NextLayout)
+            , ("M-n",       refresh)
+            , ("M-S-q",     io exitSuccess)
+            , ("M-q",       spawn "xmonad --recompile ; killall xmobar ; xmonad --restart")
+            ]
+    -- Window management keybinds.
+        window = 
+            [ ("M-<Tab>",    windows W.focusDown)
+            , ("M-j",        windows W.focusDown)
+            , ("M-k",        windows W.focusUp)
+            , ("M-m",        windows W.focusMaster)
+            , ("M-<Return>", windows W.swapMaster)
+            , ("M-S-j",      windows W.swapDown)
+            , ("M-S-k",      windows W.swapUp)
+            , ("M-h",        sendMessage Shrink)
+            , ("M-l",        sendMessage Expand)
+            , ("M-t",        withFocused $ windows . W.sink)
+            , ("M-S-f",      withFocused toggleFull)
+            ]
+    -- Spawning applications.
+        applications =
+            [ ("M-S-<Return>", spawn myTerminal)
+            , ("M-f",          spawn myBrowser)
+            , ("M-s",          spawn screenShotSelection)
+            , ("<Print>",      spawn screenShotFullscreen)
+            , ("M-p",          spawn "dmenu_run")
+            ]
+    -- Multimedia keybinds.
+        multimedia =
+            [ ("<XF86AudioPlay>",        spawn "playerctl play-pause")
+            , ("<XF86AudioPrev>",        spawn "playerctl previous")
+            , ("<XF86AudioNext>",        spawn "playerctl next")
+            , ("<XF86AudioMute>",        spawn "pactl set-sink-mute @DEFAULT_SINK@ toggle")
+            , ("<XF86AudioLowerVolume>", spawn "pactl set-sink-volume @DEFAULT_SINK@ -1.5%")
+            , ("<XF86AudioRaiseVolume>", spawn "pactl set-sink-volume @DEFAULT_SINK@ +1.5%")
+            , ("<Pause>",                spawn "amixer sset Capture toggle")
+            ]
 
 myMouseBindings :: XConfig l -> M.Map (KeyMask, Button) (Window -> X ())
 myMouseBindings XConfig {XMonad.modMask = modm} = M.fromList
- -- Set the window to floating mode and move by dragging.
-  [ ((modm, button1), \w -> focus w >> mouseMoveWindow w      >> windows W.shiftMaster)
- -- Raise the window to the top of the stack.
-  , ((modm, button2), \w -> focus w >> windows W.shiftMaster)
- -- Set the window to floating mode and resize by dragging.
-  , ((modm, button3), \w -> focus w >> mouseResizeWindow w    >> windows W.shiftMaster)
-  ]
+  -- Set the window to floating mode and move by dragging.
+    [ ((modm, button1), \w -> focus w >> mouseMoveWindow w      >> windows W.shiftMaster)
+  -- Raise the window to the top of the stack.
+    , ((modm, button2), \w -> focus w >> windows W.shiftMaster)
+  -- Set the window to floating mode and resize by dragging.
+    , ((modm, button3), \w -> focus w >> mouseResizeWindow w    >> windows W.shiftMaster)
+    ]
 
 myStartupHook :: X ()
 myStartupHook = do
-  traverse spawnOnce
-    [ "~/.fehbg"
-    , "picom"
-    , "dunst -conf $HOME/.config/dunst/dunstrc"
-    ]
-  setDefaultCursor xC_left_ptr
-  setWMName "XMonad LG3D"
+    traverse spawnOnce
+        [ "~/.fehbg"
+        , "picom"
+        , "dunst -conf $HOME/.config/dunst/dunstrc"
+        ]
+    setDefaultCursor xC_left_ptr
+    setWMName "XMonad LG3D"
 
 isInstance (ClassApp c _) = className =? c
 isInstance (TitleApp t _) = title =? t
@@ -160,57 +179,58 @@ obs     = ClassApp "obs"                   "obs"
 
 myManageHook :: XMonad.Query (Data.Monoid.Endo WindowSet)
 myManageHook = manageRules
-  where
-  -- Hides windows without ignoring it, see doHideIgnore in XMonad contrib.
-    doHide = ask >>= doF . W.delete :: ManageHook
-  -- WM_WINDOW_ROLE will be parsed with the role variable.
-    isRole = stringProperty "WM_WINDOW_ROLE"
-  -- To match multiple properties with one operator.
-    anyOf = foldl (<||>) (pure False) :: [Query Bool] -> Query Bool
-  -- To match multiple classNames with one operator.
-    match = anyOf . fmap isInstance :: [App] -> Query Bool
-  -- Checking for splash dialogs.
-    isSplash = isInProperty "_NET_WM_WINDOW_TYPE" "_NET_WM_WINDOW_TYPE_SPLASH"
-  -- Checking for pop-ups.
-    isPopup = isRole =? "pop-up"
-  -- Checking for file chooser dialog.
-    isFileChooserDialog = isRole =? "GtkFileChooserDialog" 
-  -- Managing rules for applications.
-    manageRules = composeOne
-      [ transience
-      , isDialog     -?> doCenterFloat
-      , isFullscreen -?> (doF W.focusDown <+> doFullFloat) 
-      , match [ gimp
-              , gimp2
-              , about
-              , message
-              , obs
-              ]      -?> doFloat
-      , match [ steam
-              , multimc
-              ]      -?> doCenterFloat
-      , anyOf [ isFileChooserDialog
-              , isDialog
-              , isPopup
-              , isSplash
-              ]      -?> doCenterFloat
-      ] <> composeAll
-      [ manageDocks
-      , className =? "firefox"    <&&> title    =? "File Upload" --> doFloat
-      , className =? "firefox"    <&&> title    =? "Library"     --> doCenterFloat
-      , className =? "firefox"    <&&> title    ^? "Save"        --> doFloat
-      , className =? "firefox"    <&&> resource =? "Toolkit"     --> doFloat
-      , className ^? "jetbrains-" <&&> title    ^? "Welcome to " --> doCenterFloat
-      , className ^? "jetbrains-" <&&> title    =? "splash"      --> doFloat
-      , className ^? "Visual "    <&&> isDialog                  --> doCenterFloat
-      , resource  =? "desktop_window"                            --> doIgnore
-      , resource  =? "kdesktop"                                  --> doIgnore
-      , isRole    ^? "About"      <||> isRole   ^? "about"       --> doFloat
-    -- Steam Game Fixes 
-      , className =? "steam_app_1551360" <&&> title /=? "Forza Horizon 5" --> doHide -- Prevents black screen when fullscreening.
-      , title     =? "Wine System Tray"                                   --> doHide -- Prevents Wine System Trays from taking input focus.
-      , title     ^? "Steam - News"                                       --> doHide -- I don't like the Steam news menu 
-      ]
+    where
+    -- Hides windows without ignoring it, see doHideIgnore in XMonad contrib.
+        doHide = ask >>= doF . W.delete :: ManageHook
+    -- WM_WINDOW_ROLE will be parsed with the role variable.
+        isRole = stringProperty "WM_WINDOW_ROLE"
+    -- To match multiple properties with one operator.
+        anyOf = foldl (<||>) (pure False) :: [Query Bool] -> Query Bool
+    -- To match multiple classNames with one operator.
+        match = anyOf . fmap isInstance :: [App] -> Query Bool
+    -- Checking for splash dialogs.
+        isSplash = isInProperty "_NET_WM_WINDOW_TYPE" "_NET_WM_WINDOW_TYPE_SPLASH"
+    -- Checking for pop-ups.
+        isPopup = isRole =? "pop-up"
+    -- Checking for file chooser dialog.
+        isFileChooserDialog = isRole =? "GtkFileChooserDialog" 
+    -- Managing rules for applications.
+        manageRules = composeOne
+            [ transience
+            , isDialog     -?> doCenterFloat
+            , isFullscreen -?> (doF W.focusDown <+> doFullFloat) 
+            , match [ gimp
+                    , gimp2
+                    , about
+                    , message
+                    , obs
+                    ]      -?> doFloat
+                    , match [ steam
+                    , multimc
+                    ]      -?> doCenterFloat
+                    , anyOf [ isFileChooserDialog
+                    , isDialog
+                    , isPopup
+                    , isSplash
+                    ]      -?> doCenterFloat
+            ] <> composeAll
+            [ manageDocks
+            , className =? "firefox"    <&&> title    =? "File Upload" --> doFloat
+            , className =? "firefox"    <&&> title    =? "Library"     --> doCenterFloat
+            , className =? "firefox"    <&&> title    ^? "Save"        --> doFloat
+            , className =? "firefox"    <&&> resource =? "Toolkit"     --> doFloat
+            , className ^? "jetbrains-" <&&> title    ^? "Welcome to " --> doCenterFloat
+            , className ^? "jetbrains-" <&&> title    =? "splash"      --> doFloat
+            , className ^? "Visual "    <&&> isDialog                  --> doCenterFloat
+            , resource  =? "desktop_window"                            --> doIgnore
+            , resource  =? "kdesktop"                                  --> doIgnore
+            , isRole    ^? "About"      <||> isRole   ^? "about"       --> doFloat
+            , "_NET_WM_WINDOW_TYPE" `isInProperty` "_KDE_NET_WM_WINDOW_TYPE_OVERRIDE" --> doIgnore <> doRaise
+        -- Steam Game Fixes 
+            , className =? "steam_app_1551360" <&&> title /=? "Forza Horizon 5" --> doHide -- Prevents black screen when fullscreening.
+            , title     =? "Wine System Tray"                                   --> doHide -- Prevents Wine System Trays from taking input focus.
+            , title     ^? "Steam - News"                                       --> doHide -- I don't like the Steam news menu 
+            ]
 
 {- May be useful one day 
 doClose = ask >>= liftX . killWindow >> mempty :: ManageHook
@@ -226,57 +246,48 @@ myLayoutHook =
     $ spacingRaw False(Border w w w w) True(Border w w w w) True
     $ tiled ||| Mirror tiled ||| Full
     where
-      tiled = Tall nmaster delta ratio
-      nmaster = 1     -- Default number of windows in the master pane.
-      ratio = 1 / 2   -- Default proportion of screen occupied by master panes.
-      delta = 3 / 100 -- Percent of screen increment by when resizing panes.
-      w = 7           -- Width of pixel size between windows while tiled. 
+        tiled = Tall nmaster delta ratio
+        nmaster = 1     -- Default number of windows in the master pane.
+        ratio = 1 / 2   -- Default proportion of screen occupied by master panes.
+        delta = 3 / 100 -- Percent of screen increment by when resizing panes.
+        w = 7           -- Width of pixel size between windows while tiled. 
 
 myXmobarPP :: X PP
 myXmobarPP =
-  clickablePP $ def
-    { ppCurrent          = xmobarColor "#61AFEF" ""
-    , ppVisibleNoWindows = Just (xmobarColor "#A9B1D6" "")
-    , ppHidden           = xmobarColor "#ABB2BF" ""
-    , ppHiddenNoWindows  = xmobarColor "#6B7089" ""
-    , ppUrgent           = xmobarColor "#F7768E" "" . wrap "!" "!"
-    , ppTitle            = xmobarColor "#CCD0D8" "" . shorten 49 
-    , ppSep              = " "
-    , ppWsSep            = xmobarColor "" "#282C34:5" "  "
-    , ppLayout           = xmobarColor "#31353F" "" 
-                           . (\case
-                               "Spacing Tall"        -> "<fn=2><fc=#31353F,#282C34:7>\xe0b6</fc></fn><icon=tiled.xpm/><fn=2><fc=#31353F,#282C34:7>\xe0b4</fc></fn>"
-                               "Spacing Mirror Tall" -> "<fn=2><fc=#31353F,#282C34:7>\xe0b6</fc></fn><icon=mirrortiled.xpm/><fn=2><fc=#31353F,#282C34:7>\xe0b4</fc></fn>"
-                               "Spacing Full"        -> "<fn=2><fc=#31353F,#282C34:7>\xe0b6</fc></fn><icon=full.xpm/><fn=2><fc=#31353F,#282C34:7>\xe0b4</fc></fn>"
-                             )                     
-    }
+    clickablePP $ def
+        { ppCurrent          = xmobarColor "#61AFEF" ""
+        , ppVisibleNoWindows = Just (xmobarColor "#A9B1D6" "")
+        , ppHidden           = xmobarColor "#ABB2BF" ""
+        , ppHiddenNoWindows  = xmobarColor "#6B7089" ""
+        , ppUrgent           = xmobarColor "#F7768E" "" . wrap "!" "!"
+        , ppTitle            = xmobarColor "#CCD0D8" "" . shorten 49 
+        , ppSep              = " "
+        , ppWsSep            = xmobarColor "" "#282C34:5" "  "
+        , ppLayout           = xmobarColor "#31353F" "" 
+                               . (\case
+                                   "Spacing Tall"        -> "<fn=2><fc=#31353F,#282C34:7>\xe0b6</fc></fn><icon=tiled.xpm/><fn=2><fc=#31353F,#282C34:7>\xe0b4</fc></fn>"
+                                   "Spacing Mirror Tall" -> "<fn=2><fc=#31353F,#282C34:7>\xe0b6</fc></fn><icon=mirrortiled.xpm/><fn=2><fc=#31353F,#282C34:7>\xe0b4</fc></fn>"
+                                   "Spacing Full"        -> "<fn=2><fc=#31353F,#282C34:7>\xe0b6</fc></fn><icon=full.xpm/><fn=2><fc=#31353F,#282C34:7>\xe0b4</fc></fn>"
+                                 )                     
+        }
 
-xmobar = statusBarProp myXmobar myXmobarPP :: StatusBarConfig
+xmobar :: StatusBarConfig
+xmobar = statusBarProp myXmobar myXmobarPP 
 
-myXmobar = ("xmobar " <> myHomeDir <> "/.config/xmonad/src/xmobar.hs") :: String
+myXmobar :: String
+myXmobar = ("xmobar " ++ myHomeDir ++ "/.config/xmonad/src/xmobar.hs")
 
 myConfig = 
-  def
-    { modMask            = myModMask
-    , terminal           = myTerminal
-    , mouseBindings      = myMouseBindings
-    , borderWidth        = myBorderWidth
-    , normalBorderColor  = myNormColor
-    , focusedBorderColor = myFocusColor
-    , layoutHook         = myLayoutHook
-    , startupHook        = myStartupHook
-    , manageHook         = myManageHook
-    , handleEventHook    = myEventHook
-    , workspaces         = myWorkspaces
-    } `additionalKeysP` myAdditionalKeys
-
-main :: IO ()
-main = do
-    xmonad
-    . docks
-    . ewmhFullscreen
-    . fullscreenSupport
-    . ewmh
-    . Hacks.javaHack
-    . withEasySB xmobar def
-    $ myConfig
+    def
+        { modMask            = myModMask
+        , terminal           = myTerminal
+        , mouseBindings      = myMouseBindings
+        , borderWidth        = myBorderWidth
+        , normalBorderColor  = myNormColor
+        , focusedBorderColor = myFocusColor
+        , layoutHook         = myLayoutHook
+        , startupHook        = myStartupHook
+        , manageHook         = myManageHook
+        , handleEventHook    = myEventHook
+        , workspaces         = myWorkspaces
+        } `additionalKeysP` myAdditionalKeys
